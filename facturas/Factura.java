98df3cecbba9ca1b79e0b3079157325aff886f5b
Rober <robertofreire90@gmail.com>
2020-02-20 20:20:28 +0100

    Visibilidad de estado cambiada a protected

--- facturas/Factura.java
+++ facturas/Factura.java
@@ -3,13 +3,13 @@ public class Factura {
     public int num;
     public float base;
     public float tipoIva;
-    public Estados estado;
+    protected Estados estado;
 
     public static void main(String[] args) {}
 
     void cambiarEstado(Estados estado) {}
 
     float getTotal() {}
-    
+
     float getCantidadIva() {}
 }
