823377f473e8133a2571aaf00c41f4111e68b6ce
Rober <robertofreire90@gmail.com>
2020-02-20 19:26:33 +0100

    Esqueleto class Factura

---
+++ facturas/Factura.java
@@ -0,0 +1,11 @@
+public class Factura {
+
+    public int num;
+    public float base;
+    public float tipoIva;
+    // public Estados estado; Supuestamente no esta creado el enum pero en realidad si xD.
+
+    public static void main(String[] args) {
+
+    }
+}
